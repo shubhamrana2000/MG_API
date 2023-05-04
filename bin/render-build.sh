41315ade1a2009fcba2cd88a05f65f70a50faa8c
shubhamrana2000 <sr02012000@gmail.com>
2023-05-04 18:17:26 +0530

    changes for production

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,9 +2,9 @@
 set -o errexit
 
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
-bundle exec rails db:migrate
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
 
 #if you have seeds to run add:
 # bundle exec rails db:seed
